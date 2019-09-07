b52f9866670a08c65bfcc9f0bfff542d36333a05
AhmedAfify <a.heshamafify@gmail.com>
2019-09-07 17:27:10 -0500

    initial commit

---
+++ src/main/java/com/devcom/DevComApplication.java
@@ -0,0 +1,13 @@
+package com.devcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevComApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevComApplication.class, args);
+	}
+
+}
